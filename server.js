52609d952dd1fed722550a7710d908438cd0e40d
Akshar Dubey <aksharansh123@gmail.com>
2024-08-05 10:26:08 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
+// acquire express
+
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
 //dot config
 dotenv.config();
 
-//mongodb connection
+//Mongo db connection
 connectDB();
 
-//rest object
+//rest object is created
 const app = express();
+var corsOptions = {
+  origin: "*",
+  //optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-//middlewares
+app.get("/products/:id", cors(corsOptions), function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for only example.com." });
+});
+
+// middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-//routes
-// 1 test route
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blood Bank Backend!');
+});
+
+// ROUTES
 app.use("/api/v1/test", require("./routes/testRoutes"));
-app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"))
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
-app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 //port
 const PORT = process.env.PORT || 8080;
@@ -32,7 +46,7 @@ const PORT = process.env.PORT || 8080;
 //listen
 app.listen(PORT, () => {
   console.log(
-    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
-      .bgBlue.white
+    `Node server running In ${process.env.DEV_MODE} Modeon Port
+   ${process.env.PORT}`.bgBlue.white
   );
 });
